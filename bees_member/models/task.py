e62c9bdc96884c31d38cb743dacacd0e4d181701
Thibault Francois <francois.th@gmail.com>
2015-11-01 14:42:00 +0100

    [EX] add new field on res.users and workflow on task

--- bees_member/models/task.py
+++ bees_member/models/task.py
@@ -13,4 +13,24 @@ class BeesTask(models.Model):
     duration = fields.Float("Durée")
     type_id = fields.Many2one("bees.task.type", "Type")
     responsible_id = fields.Many2one("res.users", string="Responsable")
+    state = fields.Selection([('draft', 'Draft'), 
+                              ('confirmed', 'Confirmed'), 
+                              ('done', 'Done'), 
+                              ('absent', 'Absent')], default='draft', readonly=True)
     
+    @api.one                        
+    def confirm(self):
+        self.state = 'confirmed'
+
+    @api.one                        
+    def absent(self):
+        self.state = 'absent'
+
+    @api.one                        
+    def done(self):
+        self.state = 'done'
+        users = self.env['res.users'].search([('member', '=', True)])
+        for user in users:
+            print user.login
+            print user.nb_part
+
